9d03fc3e3d3ce1f6539461128d274855d39ce507
abood qabaha <abdalluhqabaha@gmail.com>
2025-06-23 16:40:53 +0300

    create a method that hanle  data with localsrorage

---
+++ App.js
@@ -0,0 +1,7 @@
+const setLocalStorage = (key, value) => {
+  if (value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    return JSON.parse(localStorage.getItem(key)) || [];
+  }
+};
